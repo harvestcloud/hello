3fb6fe6ed2a173c2649148907fa1d9c652bad91f
Tom Haskins-Vaughan <tomhv@users.noreply.github.com>
2020-01-17 22:38:11 -0500

    Now reading message from server

---
+++ client/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <div className="logo">
+      <h1>Harvest Cloud</h1>
+      <h2>More local food</h2>
+    </div>
+  );
+}
+
+export default Logo;
